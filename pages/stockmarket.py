0e146eb8703885b87de651a425dc1a4803be26af
smart-applications-de <aineah.smart.application@gmail.com>
2025-01-08 21:51:02 +0100

    Update stockmarket.py

--- pages/stockmarket.py
+++ pages/stockmarket.py
@@ -41,6 +41,8 @@ if germin_key  and serp_key:
     @st.cache_resource
     def get_performance():
         try:
+             DailyQuote("AMD")
+             AllStocksPerformnace()
             df_mega = pd.read_csv("./history/data/mega_capCompanies.csv")
             df_stock=pd.DataFrame()
 
