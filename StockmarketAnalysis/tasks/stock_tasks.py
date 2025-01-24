b3c03adc146126cc874c9a4db8d804e3964afcc1
smart-applications-de <aineah.smart.application@gmail.com>
2025-01-24 23:05:26 +0100

    Update stock_tasks.py

--- StockmarketAnalysis/tasks/stock_tasks.py
+++ StockmarketAnalysis/tasks/stock_tasks.py
@@ -37,7 +37,7 @@ class StockmarketTasks():
         earningsGrowth= p.info['earningsGrowth']
         revenueGrowth = p.info['revenueGrowth']
 
-        description_FR = dedent(f"""
+        description_FR = dedent(f""" You are working for the MOST IMPORTANT customer and you have to impress  him with the BEST Results!. 
             Conduct a thorough analysis of {company_stock}'s stock 
             financial health and market performance by reading and interpreting the csv files .
             Extract these KPI's from the give csv files: 
@@ -76,7 +76,7 @@ class StockmarketTasks():
     def financial_analyst_task(self,agent,company_stock,company, sector,summary,industry,website) :
         date=pd.to_datetime('today').date()
         yr = pd.to_datetime('today').year
-        description_FA = dedent(f"""
+        description_FA = dedent(f""" You are working for the MOST IMPORTANT customer and you have to impress  him with the BEST Results !.
             Conduct a thorough analysis of {company_stock}'s stock financial health and market performance based on internet research. The current year is {yr} and current date: {date}.
              Also, analyze the stock's performance in comparison
             to its industry peers and overall market trends. Make sure to use the latest data and information.  
@@ -109,6 +109,7 @@ class StockmarketTasks():
 
 
         description_re = dedent(f"""
+               You are working for the MOST IMPORTANT customer and you have to impress  him with the BEST Results !.
                 Collect and summarize recent news articles, press
                 releases, and market analyses related to the {company_stock} stock and its industry.The current year is {yr} and current date: {date}.
                 Pay special attention to any significant events, market sentiments, and analysts' opinions. 
@@ -137,6 +138,7 @@ class StockmarketTasks():
         date=pd.to_datetime('today').date()
         yr = pd.to_datetime('today').year
         description_F_analysis = dedent(f"""
+            You are working for the MOST IMPORTANT customer and you have to impress  him with the BEST Results !.
             Analyze the latest news and sentiments  for
              the stock {company_stock} and company name: {company}  in question as well as 10-Q and 10-K filings. The current year is {yr} and current date: {date}.
             Focus on key sections like Management's Discussion and analysis,
